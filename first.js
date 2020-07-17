689195e713edc4c3d5dbe0ec5a3d27f154b8413a
Dhanashreee-9 <68267346+Dhanashreee-9@users.noreply.github.com>
2020-07-17 09:47:56 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,57 @@
+console.log("This are methods of array and strings");
+
+//Arrays
+//JavaScript arrays are used to store multiple values in a single variable.
+
+let arr=['1','20','hello','Fun',['1','2']];
+console.log(arr);
+
+console.log(Array.isArray(arr));
+
+console.log(Array.isArray(Text));
+
+console.log(arr[3]);
+
+console.log(arr[4]);
+
+alert(arr.indexOf('hello'))
+
+let arr2=['Pune','Mumbai','200','12'];
+console.log(arr.concat(arr2));
+
+arr2.unshift('sweet');
+console.log(arr2);
+
+arr2.push('Car');
+console.log(arr2);
+
+arr2.pop('Car');
+console.log(arr2);
+
+console.log(arr2.reverse());
+console.log(arr2);
+
+
+//Strings
+//A JavaScript string stores a series of characters like "ABCD cahjsnff jfsuyf".
+ 
+let str='Hello I am learning JavaScript.'
+ console.log(str);
+
+ console.log(str.charAt('1'));
+ console.log(str.charAt('8'));
+
+ console.log(str[10]);
+ console.log(str[17]);
+
+ console.log(str.indexOf('I'));
+ console.log(str.indexOf('S'));
+
+ console.log(str.endsWith("JavaScript"));
+ console.log(str.split('e'));
+
+ console.log(str.split(' '));
+
+
+
+
