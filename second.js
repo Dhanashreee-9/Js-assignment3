7ddc44598370833b7bf88bfd6fbf4be90c8a7fa6
Dhanashreee-9 <68267346+Dhanashreee-9@users.noreply.github.com>
2020-07-23 12:55:19 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,23 @@
+console.log("Take input from user");
+
+
+let age=window.prompt("Enter your age:")
+//console.log(age);
+
+// switch(age){
+//     case(age >=21):
+//     console.log(`Your age is ${age} so You can drink`);
+//     break;
+
+//     default:
+//     console.log(`Your age is ${age} so You cannot drink`);
+//     break;
+// }
+
+
+if(age>=21){
+    console.log(`Your age is ${age} so You can drink`);
+}
+else{
+    console.log(`Your age is ${age} so You cannot drink`);
+}
---
+++ third.js
@@ -0,0 +1,5 @@
+console.log("Takes input from user and logs into console");
+
+let a=window.prompt("Enter anthying:");
+console.log(`${a}`);
+console.log(typeof (a));
